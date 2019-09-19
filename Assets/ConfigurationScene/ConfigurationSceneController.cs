b9df3aed896a325f941d98a976ff653c6d53e247
Edgar Lopez-Garcia <edgarylop@gmail.com>
2019-09-18 20:02:04 -0700

    Add main menu and configuration scenes

---
+++ Assets/ConfigurationScene/ConfigurationSceneController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace BarajaDeLoteria.ConfigurationScene
+{
+    public class ConfigurationSceneController : MonoBehaviour
+    {
+        public void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
+
---
+++ Assets/MainMenu/MenuCanvasController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace BarajaDeLoteria.MainMenu
+{
+    public class MenuCanvasController : MonoBehaviour
+    {
+        public void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
