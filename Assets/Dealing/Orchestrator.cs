feb94a7d0a9ad7e44d2c052df2fb10a95d856dc2
Edgar Lopez-Garcia <edgarylop@gmail.com>
2019-12-26 15:25:46 -0800

    Change dealing logic to centralized orchestrator

---
+++ Assets/Dealing/Orchestrator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orchestrator : MonoBehaviour
+{
+    [SerializeField] private Dealer dealer;
+    [SerializeField] private CardsManager cardsManager;
+    [SerializeField] private UIController uiController;
+    [SerializeField] private StateMachineManager stateMachine;
+
+    private void OnEnable()
+    {
+        stateMachine.OnStateEnterDealing += dealer.StartDealing;
+
+        dealer.OnDrawCard.AddListener(cardsManager.HandleCardDraw);
+        dealer.OnCardTimeReached.AddListener(cardsManager.HandleCardTimeReached);
+        dealer.OnDealingTimeStep.AddListener(uiController.UpdateTimeIndicator);
+
+        cardsManager.OnCardDiscardEnd.AddListener(dealer.DealNextCard);
+    }
+
+    private void OnDisable()
+    {
+        stateMachine.OnStateEnterDealing -= dealer.StartDealing;
+
+        dealer.OnDrawCard.RemoveListener(cardsManager.HandleCardDraw);
+        dealer.OnCardTimeReached.RemoveListener(cardsManager.HandleCardTimeReached);
+        dealer.OnDealingTimeStep.RemoveListener(uiController.UpdateTimeIndicator);
+
+        cardsManager.OnCardDiscardEnd.RemoveListener(dealer.DealNextCard);
+    }
+}
